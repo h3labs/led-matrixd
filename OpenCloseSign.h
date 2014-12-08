ca58160ed5cae7639913b18c8644de9291fc6975
victor-fdez <victor.j.fdez@gmail.com>
2014-12-08 13:32:15 -0700

    mini-fix

--- OpenCloseSign.h
+++ OpenCloseSign.h
@@ -19,40 +19,6 @@ namespace ledMatrixD {
     void run();
     void notify(std::string fileName, int event);
     private:
-    void showSign(bool first, bool newIsOpen)
-    {
-      if(!first){
-        if(newIsOpen != this->isOpen){
-          //remove old led matrix sign
-          delete image_gen;
-          std::cout << "removing old scroller image" << std::endl;
-        }
-      }
-      //create new matrix and display for either open or close
-      if(newIsOpen != this->isOpen){
-        if(newIsOpen){
-          ImageScroller *scroller = new ImageScroller(this->canvas,
-              demo == 1 ? 1 : -1,
-              scroll_ms);
-          if (!scroller->LoadPPM("signcfg/open.ppm")){
-            fprintf(stderr, "failed to load ppm");
-            exit(EXIT_FAILURE);
-          }
-          this->image_gen = scroller;
-          std::cout << "Show \'open\'" << std::endl;
-        }else{
-          ImageScroller *scroller = new ImageScroller(this->canvas,
-              demo == 1 ? 1 : -1,
-              scroll_ms);
-          if (!scroller->LoadPPM("signcfg/closed.ppm")){
-            fprintf(stderr, "failed to load ppm");
-            exit(EXIT_FAILURE);
-          }
-          this->image_gen = scroller;
-          std::cout << "Show \'closed\'" << std::endl;
-        }
-        this->image_gen->Start();
-      }
-    } 
+    void showSign(bool first, bool newIsOpen);
   };
 }
