cc8d731f4f94d1fcd41af633bfb87db9188663db
victor-fdez <victor.j.fdez@gmail.com>
2014-12-09 17:08:46 -0700

    minifix

--- ScriptRunner.h
+++ ScriptRunner.h
@@ -2,8 +2,11 @@
 #ifndef __SCRIPT_RUNNER_H__
 #define __SCRIPT_RUNNER_H__
 
-#include "file-status-notification.h"
 #include <pthread.h>
+
+#include <string>
+
+#include "file-status-notification.h"
 
 namespace ledMatrixD {
   class ScriptRunner : public FileStatusNotifee {
